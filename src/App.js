84a4c67882cae08c97cd7e0322a2c9a8e0b06b9c
Clemens Dinkel <66182913+ClemensDinkel@users.noreply.github.com>
2022-04-20 09:22:23 +0200

    Update README.md

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
+import { useState } from 'react';
 import Draggable from 'react-draggable';
 
 function App() {
+  const [warn, setWarn] = useState(false)
+  const [warn2, setWarn2] = useState(false)
   return (
     <div className="App">
       <header className="App-header">
         <Draggable>
-          <div style={{width: "100px", height: "100px", border: "4px white", borderRadius: "8px", background: "aquamarine", cursor: "grab"}}>
-            <button onPointerDown={()=> console.log("test")} style={{cursor: "pointer"}}>TEST</button>
+          <div style={{width: "300px", height: "150px", border: "4px white", borderRadius: "8px", background: warn ? "#EAD94C" : "#D1D1D1", cursor: "grab", color: "white", display: "flex", flexDirection: "column"}}>
+            onClick
+            <button onClick={()=> setWarn(!warn)} style={{cursor: "pointer", background: "#282c34", color: "white", borderRadius: "4px",  padding: "6px"}}>Switch Color</button>
+          </div>
+        </Draggable>
+        <br/>
+        <Draggable>
+          <div style={{width: "300px", height: "150px", border: "4px white", borderRadius: "8px", background: warn2 ? "#EAD94C" : "#D1D1D1", cursor: "grab", color: "white", display: "flex", flexDirection: "column"}}>
+            onPointerDown
+            <button onPointerDown={()=> setWarn2(!warn2)} style={{cursor: "pointer", background: "#282c34", color: "white", borderRadius: "4px",  padding: "6px"}}>Switch Color</button>
           </div>
         </Draggable>
       </header>
